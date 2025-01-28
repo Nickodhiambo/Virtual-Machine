dc8dce8bfc2c97153b08301cfce21396575e2511
Nick <nodhiambo01@gmail.com>
2025-01-28 14:31:23 +0300

    Fix bugs: VM is ready

--- lc3.c
+++ lc3.c
@@ -2,20 +2,17 @@
 #include <stdint.h>
 #include <signal.h>
 /* unix only*/
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <sys/termios.h>
-#include <sys/mman.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <fcntl.h>
+// #include <sys/time.h>
+// #include <sys/types.h>
+// #include <sys/termios.h>
+// #include <sys/mman.h>
 #include <Windows.h>
 #include <conio.h>
 
-#define MEMORY_MAX (1 << 16)
-uint16_t memory[MEMORY_MAX]; // 65536 memory locations
-
-/* Define LC-3 registers*/
+/* Define registers */
 enum
 {
     R_R0 = 0,
@@ -30,9 +27,147 @@ enum
     R_COND,
     R_COUNT
 };
+// Define LC-3 condition flags
+enum
+{
+    FL_POS = 1 << 0, /* P */
+    FL_ZRO = 1 << 1, /* Z*/
+    FL_NEG = 1 << 2, /* N */
+};
 
-/* Store registers in an array*/
+/* Memory mapped registers */
+enum
+{
+    MR_KBSR = 0xFE00, /* Keyboard status register */
+    MR_KBDR = 0xFE02 /* Keyboard data register */
+};
+
+/* TRAP Codes */
+enum
+{
+    TRAP_GETC = 0x20, /* Get char from keyboard, not echoed onto the terminal */
+    TRAP_OUT = 0x21, /* Output a char */
+    TRAP_PUTS = 0x22, /* Outputs a word string */
+    TRAP_IN = 0x23, /* Get char from keyboard, echoed onto the terminal */
+    TRAP_PUTSP = 0x24, /* Outputs a byte string */
+    TRAP_HALT = 0x25 /* Halt the program */
+};
+
+/* Memory storage */
+#define MEMORY_MAX (1 << 16)
+uint16_t memory[MEMORY_MAX]; // 65536 memory locations
+
+/* Register storage */
 uint16_t reg[R_COUNT];
+
+// Access keyboard
+HANDLE hStdin = INVALID_HANDLE_VALUE;
+DWORD fdwMode, fdwOldMode;
+
+void disable_input_buffering()
+{
+    hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    GetConsoleMode(hStdin, &fdwOldMode);
+    fdwMode = fdwOldMode ^ ENABLE_ECHO_INPUT ^ ENABLE_LINE_INPUT;
+    SetConsoleMode(hStdin, fdwMode);
+    FlushConsoleInputBuffer(hStdin);
+}
+
+void restore_input_buffering()
+{
+    SetConsoleMode(hStdin, fdwOldMode);
+}
+
+uint16_t check_key()
+{
+    return WaitForSingleObject(hStdin, 1000) == WAIT_OBJECT_0 && _kbhit;
+}
+
+/* Handle interrupt */
+void handle_interrupt(int signal)
+{
+    restore_input_buffering();
+    printf("\n");
+    exit(-2);
+}
+
+// Sign extend function
+uint16_t signExtend(uint16_t bit_string, int bit_count)
+{
+    if ((bit_string >> (bit_count - 1)) & 1)
+        bit_string |= (0XFFFF << bit_count);
+    return bit_string;
+}
+
+/* Swap function */
+uint16_t swap16(uint16_t x)
+{
+    return (x << 8) | (x >> 8);
+}
+
+/* Any time a value is written in a register */
+/* Update the flags to indicate its sign */
+void updateFlags(uint16_t r)
+{
+    if (reg[r] == 0)
+        reg[R_COND] = FL_ZRO;
+    else if (reg[r] >> 15) /* A one in the left-most bit indicates a negative number*/
+        reg[R_COND] = FL_NEG;
+    else
+        reg[R_COND] = FL_POS;
+}
+
+/* Read compiled assembly into a memory location */
+void read_image_file(FILE* file)
+{
+    /* Origin tells us where in memory to place our binary code */
+    uint16_t origin;
+    fread(&origin, sizeof(origin), 1, file);
+    origin = swap16(origin);
+    uint16_t max_read = MEMORY_MAX - origin;
+    uint16_t* p = memory + origin;
+    size_t read = fread(p, sizeof(uint16_t), max_read, file);
+
+    /* Swap to little endian */
+    while (read--> 0)
+    {
+        *p = swap16(*p);
+        ++p;
+    }
+}
+
+/* Read the path to a binary lc3 file */
+int read_image(const char* image_path)
+{
+    FILE* file = fopen(image_path, "rb");
+    if (!file) return 0;
+    read_image_file(file);
+    fclose(file);
+    return 1;
+}
+
+/* Memory access functions */
+void mem_write(uint16_t address, uint16_t val)
+{
+    memory[address] = val;
+}
+
+uint16_t mem_read(uint16_t address)
+{
+    if (address == MR_KBSR)
+    {
+        if (check_key())
+        {
+            memory[MR_KBSR] = (1 << 15);
+            memory[MR_KBDR] = getchar();
+        }
+        else
+        {
+            memory[MR_KBSR] = 0;
+        }
+    }
+    return memory[address];
+}
 
 /* Get LC-3 Opcodes*/
 enum
@@ -55,35 +190,16 @@ enum
     OP_TRAP,   /* execute trap */
 };
 
-// Define LC-3 condition flags
-enum
-{
-    FL_POS = 1 << 0, /* P */
-    FL_ZRO = 1 << 1, /* Z*/
-    FL_NEG = 1 << 2, /* N */
-}
-
-/* TRAP Codes */
-enum
-{
-    TRAP_GETC = 0x20, /* Get char from keyboard, not echoed onto the terminal */
-    TRAP_OUT = 0x21, /* Output a char */
-    TRAP_PUTS = 0x22, /* Outputs a word string */
-    TRAP_IN = 0x23, /* Get char from keyboard, echoed onto the terminal */
-    TRAP_PUTSP = 0x24, /* Outputs a byte string */
-    TRAP_HALT = 0x25 /* Halt the program */
-}
-
 // Main loop
 int main(int argc, const char *argv[])
 {
-    /* Take command line argumnets*/
+    /* Take command line arguments*/
     if (argc < 2)
     {
         /* Show usage string*/
         printf("lc3 [image-file]...\n");
-        exit(2)
-    }
+        exit(2);
+    };
 
     for (int j = 1; j < argc; ++j)
     {
@@ -92,7 +208,11 @@ int main(int argc, const char *argv[])
             printf("Failed to load image: %s\n", argv[j]);
             exit(1);
         }
-    }
+    };
+
+
+    signal(SIGINT, handle_interrupt);
+    disable_input_buffering();
     /* Only one condition flag should be set*/
     /* Set Z flag*/
     reg[R_COND] = FL_ZRO;
@@ -171,14 +291,17 @@ int main(int argc, const char *argv[])
                     break;
                 }
             case OP_JMP:
+            {
                 /* Loads program counter with value provided by */
                 /* bits 8-6 of instruction */
-                uint16_t r1 = (instruction >> 6) && 0x7;
+                uint16_t r1 = (instruction >> 6) & 0x7;
                 reg[R_PC] = reg[r1];
                 break;
+            }
             case OP_JSR:
+            {
                 uint16_t long_flag = (instruction >> 11) & 1;
-                reg[R_R7] = [R_PC];
+                reg[R_R7] = reg[R_PC];
                 if (long_flag)
                 {
                     uint16_t long_pc_offset = signExtend(instruction & 0x7FF, 11);
@@ -190,6 +313,7 @@ int main(int argc, const char *argv[])
                     reg[R_PC] = reg[r1];
                 }
                 break;
+            }
             case OP_LD:
             {
                 uint16_t r0 = (instruction >> 9) & 0x7;
@@ -225,14 +349,14 @@ int main(int argc, const char *argv[])
                 uint16_t r0 = (instruction >> 9) & 0x7;
                 uint16_t pc_offset = signExtend(instruction & 0x1FF, 9);
                 reg[r0] = reg[R_PC] + pc_offset;
-                update(r0);
+                updateFlags(r0);
                 break;
             }
             case OP_NOT:
             {
                 uint16_t r0 = (instruction >> 9) & 0x7;
                 uint16_t r1 = (instruction >> 6) & 0x7;
-                reg[ro] = ~reg[r1];
+                reg[r0] = ~reg[r1];
                 updateFlags(r0);
                 break;
             }
@@ -244,20 +368,25 @@ int main(int argc, const char *argv[])
                 mem_write(reg[R_PC] + pc_offset, reg[r0]);
                 break;
             }
-            case OP_STR():
+            case OP_STR:
+            {
                 /* Get source register */
                 uint16_t r0 = (instruction >> 9) & 0x7;
                 /* Get base register */
                 uint16_t r1 = (instruction >> 6) & 0x7;
                 uint16_t offset = signExtend(instruction & 0x3F, 6);
-                mem_write(reg[r1] + offset);
+                mem_write(reg[r1] + offset, reg[r0]);
                 break;
+            }
             case OP_STI:
+            {
                 uint16_t r0 = (instruction >> 9) & 0x9;
                 uint16_t pc_offset = signExtend(instruction & 0x1FF, 9);
-                mem_read(mem_read(reg[R_PC] + pc_offset, reg[r0]));
+                mem_write(mem_read(reg[R_PC] + pc_offset), reg[r0]);
                 break;
+            }
             case OP_TRAP:
+            {
                 reg[R_R7] = reg[R_PC];
 
                 switch(instruction & 0xFF)
@@ -266,7 +395,7 @@ int main(int argc, const char *argv[])
                     {
                         /* Read a single ASCII char */
                         /* Convert it from 8-bit to 16-bit */
-                        reg[R_RO] = (uint16_t)getchar();
+                        reg[R_R0] = (uint16_t)getchar();
                         updateFlags(R_R0);
                     }
                     case TRAP_OUT:
@@ -293,7 +422,7 @@ int main(int argc, const char *argv[])
                         char c = getchar();
                         putc(c, stdout);
                         fflush(stdout);
-                        reg[R_RO] = (uint16_t)c;
+                        reg[R_R0] = (uint16_t)c;
                         updateFlags(R_R0);
                     }
                     case TRAP_PUTSP:
@@ -317,95 +446,12 @@ int main(int argc, const char *argv[])
                         running = 0;
                     }
                 }
+            }
             case OP_RES:
             case OP_RTI:
             default:
-                abort()
+                abort();
             }
         }
-    shutdown()
-}
-
-/* Read compiled assembly into a memory location */
-void read_image_file(FILE* file)
-{
-    /* Origin tells us where in memory to place our binary code */
-    uint16_t origin;
-    fread(&origin, sizeof(origin), 1, file);
-    origin = swap16(origin);
-    uint16_t max_read = MEMORY_MAX - origin;
-    uint16_t* p = memory + origin;
-    size_t read = fread(p, sizeof(uint16_t), max_read, file);
-
-    /* Swap to little endian */
-    while (read--> 0)
-    {
-        *p = swap16(*p);
-        ++p;
-    }
-}
-
-/* Swap function */
-uint16_t swap16(uint16_t x)
-{
-    return (x << 8) | (x >> 8);
-}
-
-/* Read the path to a binary lc3 file */
-int read_image(const char* image_path)
-{
-    FILE* file = fopen(image_path, "rb");
-    if (!file) return 0;
-    read_image_file(file);
-    fclose(file);
-    return 1;
-}
-
-/* Memory mapped registers */
-enum
-{
-    MR_KBSR = 0xFE00, /* Keyboard status register */
-    MR_KBDR = 0xFE02 /* Keyboard data register */
-};
-
-/* Memory access functions */
-void mem_write(uint16_t address, uint16_t val)
-{
-    memory[address] = val;
-}
-
-uint16_t mem_read(uint16_t address)
-{
-    if (address == MR_KBSR)
-    {
-        if (check_key())
-        {
-            memory[MR_KBSR] = (1 << 15);
-            memory[MR_KBDR] = getchar();
-        }
-        else
-        {
-            memory[MR_KBSR] = 0;
-        }
-    }
-    return memory[address];
-}
-// Sign extend function
-uint16_t signExtend(uint16_t bit_string, int bit_count)
-{
-    if ((bit_string >> (bit_count - 1)) & 1)
-        bit_string |= (0XFFFF << bit_count);
-    return bit_string
-}
-
-/* Any time a value is written in a register */
-/* Update the flags to indicate its sign */
-void updateFlags(uint16_t r)
-{
-    if (reg[r] == 0)
-        reg[R_COND] = FL_ZRO;
-    else if (reg[r] >> 15) /* A one in the left-most bit indicates a negative number*/
-        reg[R_COND] = FL_NEG;
-    else
-        reg[R_COND] = FL_POS;
+    restore_input_buffering();
 }
